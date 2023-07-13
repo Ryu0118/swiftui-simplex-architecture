7437ef5d59d076a77a1ab34b989041a23a302d94
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-07-14 03:36:49 +0900

    add macro test

---
+++ Tests/SimplexArchitectureTests/StateBuilderMacroTests.swift
@@ -0,0 +1,125 @@
+#if canImport(SimplexArchitectureMacrosPlugin)
+import XCTest
+import SimplexArchitecture
+import SimplexArchitectureMacrosPlugin
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+
+final class StateBuilderMacroTests: XCTestCase {
+    let macros: [String: Macro.Type] = [
+        "StoreBuilder": StoreBuilder.self
+    ]
+
+    func testMacro() throws {
+        assertMacroExpansion(
+            """
+            @StoreBuilder(reducer: TestReducer())
+            struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+
+                var body: some View {
+                    EmptyView()
+                }
+            }
+            """,
+            expandedSource: """
+
+            struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+
+                var body: some View {
+                    EmptyView()
+                }
+                internal let store = Store(reducer: TestReducer())
+                internal struct States: StatesProtocol {
+                    var testState: Int = 0
+                    var testBinding: String
+                    internal static var keyPathDictionary: [PartialKeyPath<States>: PartialKeyPath<TestView>] {
+                        [\\.testState: \\.testState, \\.testBinding: \\.testBinding]
+                    }
+                }
+            }
+            """,
+            macros: macros
+        )
+
+        assertMacroExpansion(
+            """
+            @StoreBuilder(reducer: TestReducer())
+            struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+                var hoge = 0
+                var fuga = 0
+
+                var body: some View {
+                    EmptyView()
+                }
+            }
+            """,
+            expandedSource: """
+
+            struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+                var hoge = 0
+                var fuga = 0
+
+                var body: some View {
+                    EmptyView()
+                }
+                internal let store = Store(reducer: TestReducer())
+                internal struct States: StatesProtocol {
+                    var testState: Int = 0
+                    var testBinding: String
+                    internal static var keyPathDictionary: [PartialKeyPath<States>: PartialKeyPath<TestView>] {
+                        [\\.testState: \\.testState, \\.testBinding: \\.testBinding]
+                    }
+                }
+            }
+            """,
+            macros: macros
+        )
+
+        assertMacroExpansion(
+            """
+            @StoreBuilder(reducer: TestReducer())
+            public struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+                var hoge = 0
+                var fuga = 0
+
+                public var body: some View {
+                    EmptyView()
+                }
+            }
+            """,
+            expandedSource: """
+
+            public struct TestView: View {
+                @State var testState: Int = 0
+                @Binding var testBinding: String
+                var hoge = 0
+                var fuga = 0
+
+                public var body: some View {
+                    EmptyView()
+                }
+                public let store = Store(reducer: TestReducer())
+                public struct States: StatesProtocol {
+                    var testState: Int = 0
+                    var testBinding: String
+                    public static var keyPathDictionary: [PartialKeyPath<States>: PartialKeyPath<TestView>] {
+                        [\\.testState: \\.testState, \\.testBinding: \\.testBinding]
+                    }
+                }
+            }
+            """,
+            macros: macros
+        )
+    }
+}
+#endif
