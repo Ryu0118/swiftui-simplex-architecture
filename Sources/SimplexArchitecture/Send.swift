0bc3a86b277be8d5fff072722f4a24168804ad98
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-07-14 20:34:03 +0900

    Enable Task cancellation in Send.swift

--- Sources/SimplexArchitecture/Send.swift
+++ Sources/SimplexArchitecture/Send.swift
@@ -3,6 +3,7 @@ import Foundation
 public final class Send<Target: SimplexStoreView>: @unchecked Sendable where Target.Reducer.State == StateContainer<Target> {
     private let target: Target
     private var container: StateContainer<Target>
+    private var currentTask: Task<(), Never>?
 
     init(target: Target, container: StateContainer<Target>) {
         self.target = target
@@ -18,29 +19,59 @@ public final class Send<Target: SimplexStoreView>: @unchecked Sendable where Tar
         self.target = target
         self.container = StateContainer(target, reducerState: reducerState)
     }
+
+    deinit {
+        currentTask?.cancel()
+    }
 }
 
 public extension Send {
     func callAsFunction(_ action: Target.Reducer.Action) async {
         await send(action)
     }
-}
 
-extension Send {
     func callAsFunction(_ action: Target.Reducer.Action) {
-        Task {
-            await send(action)
-        }
+        send(action)
     }
 }
 
 private extension Send {
-    func send(_ action: Target.Reducer.Action) async {
-        let effectTask = target.store.reducer.reduce(into: &container, action: action)
+    func send(_ action: Target.Reducer.Action) {
+        let effectTask = reduce(action)
+        let tasks = runEffect(effectTask)
 
+        // Root of Task Tree
+        currentTask = Task {
+            await withTaskGroup(of: Void.self) { group in
+                for task in tasks {
+                    group.addTask {
+                        await task.value
+                    }
+                }
+            }
+        }
+    }
+
+    func send(_ action: Target.Reducer.Action) async {
+        let effectTask = reduce(action)
+        let tasks = runEffect(effectTask)
+        await withTaskGroup(of: Void.self) { group in
+            for task in tasks {
+                group.addTask {
+                    await withTaskCancellationHandler {
+                        await task.value
+                    } onCancel: {
+                        task.cancel()
+                    }
+                }
+            }
+        }
+    }
+
+    func runEffect(_ effectTask: EffectTask<Target.Reducer>) -> [Task<Void, Never>] {
         switch effectTask.kind {
-        case let .run(priority, operation, `catch`):
-            await Task(priority: priority ?? .medium) {
+        case .run(let priority, let operation, let `catch`):
+            let task = Task.detached(priority: priority ?? .medium) {
                 do {
                     try await operation(self)
                 } catch {
@@ -51,26 +82,32 @@ private extension Send {
                     }
                 }
             }
-            .value
+            return [task]
 
         case let .merge(actions):
+            var tasks = [Task<Void, Never>]()
             for action in actions {
-                Task {
-                    await self(action)
+                let task = Task.detached {
+                    await self.send(action)
                 }
+                tasks.append(task)
             }
-            return
+            return tasks
 
         case let .concat(actions):
-            Task {
+            let task = Task.detached {
                 for action in actions {
-                    await self(action)
+                    await self.send(action)
                 }
             }
-            return
+            return [task]
 
         case .none:
-            return
+            return []
         }
+    }
+
+    func reduce(_ action: Target.Reducer.Action) -> EffectTask<Target.Reducer> {
+        target.store.reducer.reduce(into: &container, action: action)
     }
 }
