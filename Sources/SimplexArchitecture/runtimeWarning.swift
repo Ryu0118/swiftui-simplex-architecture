c17cefdceb02f4b13353a788f03ee81508a8452b
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-07-20 19:06:05 +0900

    fix runtimeWarning

--- Sources/SimplexArchitecture/runtimeWarning.swift
+++ Sources/SimplexArchitecture/runtimeWarning.swift
@@ -1,15 +1,29 @@
 import Foundation
+import os
 
-// FIXME: Fix Runtime Error
+// https://www.pointfree.co/blog/posts/70-unobtrusive-runtime-warnings-for-libraries
 func runtimeWarning(_ message: String, file: String = #file, line: Int = #line) {
     #if DEBUG
-    DispatchQueue.global(qos: .userInteractive).async {
-        // If you got here, please check console for more info
-        print("Runtime warning: \(message): file \(file.fileName), line \(line)")
-    }
+    var info = Dl_info()
+    dladdr(
+        dlsym(
+            dlopen(nil, RTLD_LAZY),
+            "$s10Foundation15AttributeScopesO7SwiftUIE05swiftE0AcDE0D12UIAttributesVmvg"
+        ),
+        &info
+    )
+    os_log(
+        .fault,
+        dso: info.dli_fbase,
+        log: OSLog(
+            subsystem: "com.apple.runtime-issues",
+            category: "SimplexArchitecture"
+        ),
+        "%@",
+        """
+        file: \(file), line: \(line)
+        \(message)
+        """
+    )
     #endif
-}
-
-fileprivate extension String {
-    var fileName: String { URL(fileURLWithPath: self).lastPathComponent }
 }
